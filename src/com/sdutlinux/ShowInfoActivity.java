6a4a714d6f354f1169c175c0495ab98ab0fc3837
tanhehe <443016215@qq.com>
2013-11-12 14:30:02 +0800

    fix

--- src/com/sdutlinux/ShowInfoActivity.java
+++ src/com/sdutlinux/ShowInfoActivity.java
@@ -3,6 +3,7 @@ package com.sdutlinux;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
