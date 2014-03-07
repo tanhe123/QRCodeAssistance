fc91ec6a3672fa129fa13e192aca41e5a1b732ed
Tanhehe <443016215@163.com>
2014-03-07 20:27:56 +0800

    fix

--- src/com/sdutlinux/InputIssue.java
+++ src/com/sdutlinux/InputIssue.java
@@ -2,15 +2,20 @@ package com.sdutlinux;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.SharedPreferences.Editor;
 import android.view.Menu;
+import android.widget.Spinner;
 
 public class InputIssue extends Activity {
-	private 
+	private Spinner sp_teachers;
+	private Editor edt_title, edt_desc;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_input_issue);
+		
+		
 	}
 
 	@Override
